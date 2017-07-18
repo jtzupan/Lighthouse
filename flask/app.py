20daf680b856533b7f5120f0d5cbcb5622630998
Tyler Zupan <jtzupan@gmail.com>
2017-07-17 21:38:19 -0400

    added css/html for site

--- flask/app.py
+++ flask/app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('main.html')
 
 def allowed_file(filename):
     return '.' in filename and \
